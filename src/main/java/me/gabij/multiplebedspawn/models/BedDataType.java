d008bc7a13a446390e4987b5ccc48bbd0d586fa0
HappySpleen <killerfreak@gmail.com>
2026-04-25 19:12:29 -0400

    Add LegacySerializationUtils and remove commons-lang
    Extract serialization/deserialization into a new LegacySerializationUtils to centralize legacy class relocation handling (maps old package names encoded as ASCII to current package). Replace direct use of Apache Commons SerializationUtils and custom ObjectInputStream logic in BedDataType and BedsDataType with calls to the new utility. Remove the commons-lang dependency and the maven-shade-plugin from pom.xml, and mark sqlite-jdbc as provided. This consolidates legacy compatibility logic and removes an unnecessary dependency and shading configuration.

--- src/main/java/me/gabij/multiplebedspawn/models/BedDataType.java
+++ src/main/java/me/gabij/multiplebedspawn/models/BedDataType.java
@@ -1,14 +1,7 @@
 package me.gabij.multiplebedspawn.models;
 
-import org.apache.commons.lang.SerializationUtils;
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectStreamClass;
 
 public class BedDataType implements PersistentDataType<byte[], BedData> {
 
@@ -24,45 +17,11 @@ public class BedDataType implements PersistentDataType<byte[], BedData> {
 
     @Override
     public byte[] toPrimitive(BedData complex, PersistentDataAdapterContext context) {
-        return SerializationUtils.serialize(complex);
+        return LegacySerializationUtils.serialize(complex);
     }
 
     @Override
     public BedData fromPrimitive(byte[] primitive, PersistentDataAdapterContext context) {
-        try {
-            InputStream is = new ByteArrayInputStream(primitive);
-            ObjectInputStream o = new CustomObjectInputStream(is);
-            return (BedData) o.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return LegacySerializationUtils.deserialize(primitive, BedData.class);
     }
-
-    private static class CustomObjectInputStream extends ObjectInputStream {
-        public CustomObjectInputStream(InputStream in) throws IOException {
-            super(in);
-        }
-
-        @Override
-        protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
-            String className = desc.getName();
-            String oldPackageName = getAsciiPackageName();
-            if (className.startsWith(oldPackageName)) {
-                className = className.replace(oldPackageName, "me.gabij.");
-            }
-            return super.resolveClass(ObjectStreamClass.lookup(Class.forName(className)));
-        }
-
-        private String getAsciiPackageName() {
-            // old name is stored as ints
-            int[] asciiValues = {109, 101, 46, 103, 97, 98, 114, 105, 101, 108, 102, 106, 46};
-            StringBuilder sb = new StringBuilder();
-            for (int val : asciiValues) {
-                sb.append((char) val);
-            }
-            return sb.toString();
-        }
-    }
-
 }
